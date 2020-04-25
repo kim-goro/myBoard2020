694726d07eeb2d0281b2aef68f52888cbf4f9f27
찬웅 안 <cksdnd3052@gmail.com>
2020-04-25 09:56:13 +0900

    로그인 에러 부분

--- myBoard/WebContent/WEB-INF/jsp/login.jsp
+++ myBoard/WebContent/WEB-INF/jsp/login.jsp
@@ -8,7 +8,7 @@
 <link rel="stylesheet" type="text/css" href="/css/common.css" />
 </head>
 <body>
-	<div class="flexContainer flexCenter">
+	<div class="flexContainer flexCenter" style="flex-direction:column;">
 		<form class="solidForm" action="login" method="post">
 			<div class="marBottom10">
 				<input type="text" name="u_id" placeholder="아이디">
@@ -16,11 +16,32 @@
 			<div class="marBottom10">
 				<input type="password" name="u_pw" placeholder="비밀번호">
 			</div>
-			<div class="flexContainer flexCenter">
+			<div class="flexContainer flexCenter" style="height:40px;">
 				<input type="submit" value="로그인">&nbsp;&nbsp;
 				<a href="/join">회원가입</a>
 			</div>
-		</form>			
+		</form>	
+		<div style="color:red;">
+			${msg}
+		</div>		
 	</div>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- myBoard/src/kr/koreait/myboard/LoginSev.java
+++ myBoard/src/kr/koreait/myboard/LoginSev.java
@@ -14,7 +14,28 @@ import kr.koreait.myboard.vo.UserVO;
 public class LoginSev extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//이 페이지를 로그인 시도 없이 왔는지
+		
+		//로그인 시도 했다가 오류가 떠서 왔는지
+		
+		String error = request.getParameter("error");
+		if(error != null) { //로그인 실패 했음!!
+			String errorMsg = "";
+			switch(error) {
+			case "0":
+				errorMsg = "알 수 없는 에러 발생";
+				break;
+			case "2":
+				errorMsg = "아이디를 확인해 주세요";
+				break;
+			case "3":
+				errorMsg = "비밀번호를 확인해 주세요";
+				break;
+			}
+			request.setAttribute("msg", errorMsg);
+		}
+		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
 		rd.forward(request, response);
 	}
@@ -31,6 +52,7 @@ public class LoginSev extends HttpServlet {
 		param.setU_pw(u_pw);
 		
 		int result = UserDAO.doLogin(param);
+		response.sendRedirect("/login?error=" + result);
 		
 		System.out.println("result : " + result);
 	}
